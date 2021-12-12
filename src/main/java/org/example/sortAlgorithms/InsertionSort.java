159dcbcdd3128b35e86a56836a85fd929e636758
Hanif Ali <ali.hanif@outlook.com>
2021-12-12 20:10:40 +0000

    Implemented Insertion Sort algorithm to project

---
+++ src/main/java/org/example/sortAlgorithms/InsertionSort.java
@@ -0,0 +1,39 @@
+package org.example.sortAlgorithms;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class InsertionSort implements SortTemplate{
+
+    public static void main(String[] args) {
+        InsertionSort insertionSort = new InsertionSort();
+        ArrayList<Integer> array = new ArrayList<>();
+        array.add(132);
+        array.add(34);
+        array.add(1312);
+        array.add(32);
+        array.add(1);
+        array.add(3);
+        array.add(569);
+        System.out.println(insertionSort.sorter(array));
+    }
+    @Override
+    public ArrayList<Integer> sorter(ArrayList<Integer> listOfRandomNumbers) {
+
+        for (int i = 1; i < listOfRandomNumbers.size(); i++) {
+            int valuesBefore = i ;
+            while (listOfRandomNumbers.get(i) <= listOfRandomNumbers.get(valuesBefore) ){
+                if (valuesBefore - 1 == -1) {
+                    listOfRandomNumbers.add(valuesBefore, listOfRandomNumbers.remove(i));
+                    break;
+                }
+                else if (listOfRandomNumbers.get(i) > listOfRandomNumbers.get(valuesBefore - 1)) {
+                    listOfRandomNumbers.add(valuesBefore, listOfRandomNumbers.remove(i));
+                    break;
+                }
+                valuesBefore -= 1;
+            }
+        }
+        return listOfRandomNumbers;
+    }
+}
