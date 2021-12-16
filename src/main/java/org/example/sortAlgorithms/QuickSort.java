23117c4dca6406fe9bb9a9bf9b5916cefd7a8e1b
Hanif Ali <ali.hanif@outlook.com>
2021-12-16 13:39:45 +0000

    QuickSort fixed and working,

--- src/main/java/org/example/sortAlgorithms/QuickSort.java
+++ src/main/java/org/example/sortAlgorithms/QuickSort.java
@@ -12,67 +12,41 @@ public class QuickSort implements Sorter {
         array.add(132);
         array.add(34);
         array.add(1312);
+        array.add(3432);
+        array.add(22);
+        array.add(334);
+        array.add(34);
+        array.add(1312);
         array.add(32);
         array.add(1);
-        array.add(3);
 
         QuickSort quickSort = new QuickSort();
         System.out.println(quickSort.sorter(array));
     }
     @Override
     public ArrayList<Integer> sorter(ArrayList<Integer> listOfRandomNumbers) {
-
-
-        return quickSorter(listOfRandomNumbers, 0, listOfRandomNumbers.size());
+        return quickSort(listOfRandomNumbers, 0, listOfRandomNumbers.size());
     }
-    private ArrayList quickSorter (ArrayList<Integer> list, int low, int high){
-        list = listArrangerToSelectPivot(list);
-        int pivot = list.size()/2;
-
-
-        int leftValue = 0;
-        int rightValue = 0;
-        while (leftValue != -1 && rightValue != -1){
-            leftValue = -1;
-            rightValue = -1;
-
-            for (int i = low; i < pivot; i++) {
-                if( list.get(i) > list.get(pivot)) {
-                    leftValue = i;
-                    break;
-                }
-            }
-            for (int i = high - 1; i > pivot; i--) {
-                if( list.get(i) < list.get(pivot)) {
-                    rightValue = i;
-                    break;
-                }
-            }
-            if (leftValue != -1 && rightValue != -1)
-                Collections.swap(list, leftValue, rightValue);
-            if (leftValue != -1)
-                Collections.swap(list, leftValue, pivot);
-            if (leftValue != -1 && rightValue != 0)
-                Collections.swap(list, pivot, rightValue);
+    //recursive method used to divide list repeatedly in half
+    private ArrayList quickSort (ArrayList<Integer> list, int low, int high){
+        if (low < high){
+            int pivotLocation = partOfList(list, low, high);
+            quickSort(list, low, pivotLocation);
+            quickSort(list,pivotLocation+1,high);
         }
-        quickSorter(list, low, pivot);
-        quickSorter(list, pivot, high);
-
         return list;
+    }
+    //sort part of list by swapping numbers around
+    private int partOfList(ArrayList<Integer> list, int low, int high){
+        int pivot = low;
 
+        //goes through list checking if number are greater than pivot, places number below pivot
+        for (int i = low + 1; i < high; i++) {
+            if(list.get(i) < list.get(pivot)){
+                Collections.swap(list, i, low);
+            }
+        }
+        return (pivot);
     }
 
-
-    private ArrayList<Integer> listArrangerToSelectPivot(ArrayList<Integer> selectedList){
-        int[] median = new int[3];
-        median[0] = selectedList.get(0);
-        median[1] = selectedList.get(selectedList.size()/2);
-        median[2] = selectedList.get(selectedList.size()-1);
-        Arrays.sort(median);
-        selectedList.set(0, median[0]);
-        selectedList.set(0, selectedList.size()/2);
-        selectedList.set(0, selectedList.size());
-
-        return selectedList;
-    }
 }
